a8c9068b803e79b894d061d79e87e5c1ae6b9385
Kinyanjui Wangonya <Kinyanjui Wangonya>
2017-03-15 12:38:33 +0300

    Add buttons, connect with http get

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,28 +16,46 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('lightControlCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('lightControlCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $http, $stateParams) {
 
+    $scope.setLightRelay = function(analogID, value) {
 
+                var url = "http://192.168.0.2/analog/" + analogID + "/" + value
+
+                $http.get(url);
+
+        }
 }])
    
-.controller('airConditioningCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('airConditioningCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $http, $stateParams) {
 
+    $scope.setAirRelay = function(analogID, value) {
 
+                var url = "http://192.168.0.2/analog/" + analogID + "/" + value
+
+                $http.get(url);
+
+        }
 }])
    
-.controller('securityControlCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('securityControlCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $http, $stateParams) {
 
+    $scope.setSecurityRelay = function(analogID, value) {
 
+                var url = "http://192.168.0.2/analog/" + analogID + "/" + value
+
+                $http.get(url);
+
+        }
 }])
    
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
